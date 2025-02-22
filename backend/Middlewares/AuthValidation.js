93014db9e2ba1a75952fac077a787616a6a01ede
Perwez087 <parwez4568@gmail.com>
2025-02-22 10:55:52 +0530

    update

--- backend/Middlewares/AuthValidation.js
+++ backend/Middlewares/AuthValidation.js
@@ -1,28 +1,36 @@
 const Joi = require("joi");
 
 const signupValidation = (req, res, next) => {
-  const schema = joi.object({
+  const schema = Joi.object({
     name: Joi.string().min(3).max(100).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(4).max(100).required(),
   });
-  const { error } = schema.validate(req.body);
+
+  const { error } = schema.validate(req.body, { abortEarly: false });
 
   if (error) {
-    return res.status(400).json({ message: "Bad request", error });
+    return res.status(400).json({ 
+      message: "Validation error", 
+      errors: error.details.map((err) => err.message) 
+    });
   }
   next();
 };
 
 const loginValidation = (req, res, next) => {
-  const schema = joi.object({
+  const schema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(4).max(100).required(),
   });
-  const { error } = schema.validate(req.body);
+
+  const { error } = schema.validate(req.body, { abortEarly: false });
 
   if (error) {
-    return res.status(400).json({ message: "Bad request", error });
+    return res.status(400).json({ 
+      message: "Validation error", 
+      errors: error.details.map((err) => err.message) 
+    });
   }
   next();
 };
